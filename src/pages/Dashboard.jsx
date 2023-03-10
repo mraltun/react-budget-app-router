3f09f64e41708b9f017df89e0469dd9638307342
Murat Altun <mraltun@gmail.com>
2023-03-10 17:20:53 +0300

    Added AddBudgetForm

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,12 +1,14 @@
 import { useLoaderData } from "react-router-dom";
 import { toast } from "react-toastify";
+import AddBudgetForm from "../components/AddBudgetForm";
 import Intro from "../components/Intro";
 import { fetchData } from "../helpers";
 
 // Loader
 export const dashboardLoader = () => {
   const userName = fetchData("userName");
-  return { userName };
+  const budgets = fetchData("budgets");
+  return { userName, budgets };
 };
 
 // Action
@@ -23,11 +25,27 @@ export const dashboardAction = async ({ request }) => {
 };
 
 const Dashboard = () => {
-  const { userName } = useLoaderData();
+  const { userName, budgets } = useLoaderData();
 
   return (
     <>
-      {userName ? <p>{userName}</p> : <Intro />}
+      {userName ? (
+        <div className='dashboard'>
+          <h1>
+            Welcome back <span className='accent'>{userName}</span>
+          </h1>
+          <div className='grid-sm'>
+            {/* {budgets ? () : ()} */}
+            <div className='grid-lg'>
+              <div className='flex lg'>
+                <AddBudgetForm />
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <Intro />
+      )}
       Dashboard
     </>
   );
