89c98bf5731ec9b6578cb516cce328797ade1067
Murat Altun <mraltun@gmail.com>
2023-03-11 15:38:39 +0300

    Handle multiple form submissions

--- src/helpers.js
+++ src/helpers.js
@@ -3,6 +3,22 @@ export const fetchData = (key) => {
   return JSON.parse(localStorage.getItem(key));
 };
 
+// Create budget
+export const createBudget = ({ name, amount }) => {
+  const newItem = {
+    id: crypto.randomUUID(),
+    name,
+    createdAt: Date.now(),
+    amount: +amount,
+  };
+
+  const existingBudgets = fetchData("budgets") ?? [];
+  return localStorage.setItem(
+    "budgets",
+    JSON.stringify([...existingBudgets, newItem])
+  );
+};
+
 // Delete item
 export const deleteItem = ({ key }) => {
   return localStorage.removeItem(key);
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,7 +2,7 @@ import { useLoaderData } from "react-router-dom";
 import { toast } from "react-toastify";
 import AddBudgetForm from "../components/AddBudgetForm";
 import Intro from "../components/Intro";
-import { fetchData } from "../helpers";
+import { createBudget, fetchData } from "../helpers";
 
 // Loader
 export const dashboardLoader = () => {
@@ -14,13 +14,24 @@ export const dashboardLoader = () => {
 // Action
 export const dashboardAction = async ({ request }) => {
   const data = await request.formData();
-  const formData = Object.fromEntries(data);
+  const { _action, ...values } = Object.fromEntries(data);
 
-  try {
-    localStorage.setItem("userName", JSON.stringify(formData.userName));
-    return toast.success(`Welcome, ${formData.userName}`);
-  } catch (error) {
-    throw new Error("There was a problem creating your account");
+  if (_action === "newUser") {
+    try {
+      localStorage.setItem("userName", JSON.stringify(values.userName));
+      return toast.success(`Welcome, ${values.userName}`);
+    } catch (error) {
+      throw new Error("There was a problem creating your account");
+    }
+  }
+
+  if (_action === "createBudget") {
+    try {
+      createBudget({ name: values.newBudget, amount: values.newBudgetAmount });
+      return toast.success("Budget created!");
+    } catch (error) {
+      throw new Error("There was a problem creating your budget");
+    }
   }
 };
 
