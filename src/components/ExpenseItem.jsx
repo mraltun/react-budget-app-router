96fc37eb9e994f55b9c1f2528ccaf86ac37920a5
Murat Altun <mraltun@gmail.com>
2023-03-12 18:19:14 +0300

    Added budget category

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,10 +1,21 @@
-import { formatCurrency, formatDateToLocaleString } from "../helpers";
+import {
+  formatCurrency,
+  formatDateToLocaleString,
+  getAllMatchingItems,
+} from "../helpers";
 
 const ExpenseItem = ({ expense }) => {
+  const budget = getAllMatchingItems({
+    category: "budgets",
+    key: "id",
+    value: expense.budgetId,
+  });
+
   return (
     <>
       <td>{expense.name}</td>
       <td>{formatCurrency(expense.amount)}</td>
+      <td>{formatDateToLocaleString(expense.createdAt)}</td>
       <td>{formatDateToLocaleString(expense.createdAt)}</td>
     </>
   );
